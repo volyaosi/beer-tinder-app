34e57ea24044348edda400f0e41fb92e24276430
Olha Salatova <olga.sharpmind@gmail.com>
2021-03-28 11:39:24 +0200

    feat: responsive design for viewControls

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -5,15 +5,16 @@ import "./Filter.css";
 export const Filter = ({ filterObj, handleFilter }) => {
   return (
     <div className="filter">
+      <h3>Styles:</h3>
       {Object.keys(filterObj).map((style, i) => {
         return (
-          <label key={i}>
+          <label key={i} className="filter__item">
             <input
               type="checkbox"
               onChange={(e) => handleFilter(e.target)}
               checked={filterObj[style]}
               name={style}
-              className="filter__item"
+              className="filter__checkbox"
             />
             {style}
           </label>
